430a59646b4a8ef2aede726c61f1102b53a0ea18
Brockley <mbrockley@ada.cm.cluster>
2020-07-19 12:12:00 -0400

    changed memory parameters for prodigal conversions

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -3,5 +3,5 @@
 
 
 snakemake   \
-        --jobs 40 --use-conda  \
-        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=Brockley.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes} --mail-user=mbrockley@middlebury.edu --mail-type=ALL" &
+        --jobs 60 --use-conda  \
+        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=Brockley.{rule}.{wildcards} --mem={cluster.mem}gb --mem-per-cup={cluster.mem_cpu}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes} --mail-user=mbrockley@middlebury.edu --mail-type=ALL" &
