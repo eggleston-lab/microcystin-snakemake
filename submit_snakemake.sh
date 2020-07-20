8a23df76337daf28fc7d2d4713fdfb5ef6e89cb5
Brockley <mbrockley@ada.cm.cluster>
2020-07-20 09:09:16 -0400

    HPC mem per cpu configuration

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -4,4 +4,4 @@
 
 snakemake   \
         --jobs 60 --use-conda  \
-        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=Brockley.{rule}.{wildcards} --mem={cluster.mem}gb --mem-per-cup={cluster.mem_cpu}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes} --mail-user=mbrockley@middlebury.edu --mail-type=ALL" &
+        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=Brockley.{rule}.{wildcards} --mem={cluster.mem}gb --mem-per-cpu={cluster.mem_cpu} --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes} --mail-user=mbrockley@middlebury.edu --mail-type=ALL" &
