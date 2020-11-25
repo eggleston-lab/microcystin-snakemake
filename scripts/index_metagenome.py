54dc7f15f1ba951157f885178b1990efb036ad97
Brockley <mbrockley@ada.cm.cluster>
2020-11-25 09:35:22 -0500

    adjusted write and memory parameters for indexing metagenomes

--- scripts/index_metagenome.py
+++ scripts/index_metagenome.py
@@ -2,28 +2,33 @@
 
 #define function to carry out metagenome indexing
 
-from Bio import SeqIO, SeqRecord
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 import os
 
-def index_metagenome(metagenome):
+def index_metagenome(metagenome, cleaned_file):
+	data_to_write = []
 	
 	#read in metagenome as a list of sequences
 	with open(metagenome, "r") as fasta:
 		sequences = list(SeqIO.parse(fasta, "fasta"))
+	fasta.close()
 	
 	#loop through sequences changing the sequence name and ID
 	for i in range(0, len(sequences)):
 		
 		#modify the sequence ID with the sequence index
 		sequences[i].id = str(sequences[i].id) + ":" + str(i)
-		sequences[i].name = str(sequences[i].name) + ":" + str(i)
+		
+		#build sequence record
+		seq_obj = SeqRecord(sequences[i].seq, id = sequences[i].id)
+		data_to_write.append(seq_obj)
 	
-	print(sequences[0].id)
-	print(sequences[0].name)
-	print(sequences[0].seq)
-	from Bio.SeqRecord import SeqRecord
-	print(sequences[0].format("fasta"))
+	#write the data to a new fasta file
+	with open(cleaned_file, "w") as index_fasta:
+		SeqIO.write(data_to_write, "fasta")
+	index_fasta.close()
 
 
-index_metagenome("/storage/eggleston-research/metagenomes/CB_WirMetaG_FD1.proteins.faa")
+index_metagenome(snakemake.input, snakemake.output)
 	
